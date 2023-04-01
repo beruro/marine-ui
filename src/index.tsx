bc3579fdde3a47e76767c832da0d795abee4d288
Fish <junyu.wei@foxmail.com>
2023-04-01 17:25:13 +0800

    two

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
+// export { default as Button } from "./components/Button";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
